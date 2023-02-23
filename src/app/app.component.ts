e3e6cc708b532c612e414f8f94ed5bdef351ee51
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-02-22 21:30:39 -0300

    create card and menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pipeApp';
+
+  nombre: string = 'Martin ENRIquez';
+
+  mostrarNombre() {
+    return console.log(this.nombre);
+  }
 }
