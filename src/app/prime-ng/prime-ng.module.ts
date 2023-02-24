42bd758f43ef94da4733406a747c00393a4196eb
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-02-23 23:50:42 -0300

    menu-bar component

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [CardModule, ButtonModule],
+  exports: [CardModule, ButtonModule, MenubarModule],
 })
 export class PrimeNgModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { MenuComponent } from './menu/menu.component';
 
-
-
 @NgModule({
-  declarations: [
-    MenuComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MenuComponent],
+  exports: [MenuComponent],
+  imports: [CommonModule, PrimeNgModule],
 })
-export class SharedModule { }
+export class SharedModule {}
