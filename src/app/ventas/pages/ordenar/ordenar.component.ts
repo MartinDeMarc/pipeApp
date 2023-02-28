836753639b4f0d67c02b1908a00f345ff1cd827f
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-02-28 14:06:44 -0300

    table with superheroes and data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
   EnMayusculas: boolean = true;
 
-  constructor() {}
+  heroes: Heroe[] = [
+    { nombre: 'Superman', vuela: true, color: Color.azul },
+    { nombre: 'Batman', vuela: false, color: Color.negro },
+    { nombre: 'Robin', vuela: false, color: Color.verde },
+    { nombre: 'Derdevil', vuela: false, color: Color.rojo },
+    { nombre: 'Linter Verde', vuela: true, color: Color.verde },
+  ];
 
-  ngOnInit(): void {}
   handleClick() {
     this.EnMayusculas = !this.EnMayusculas;
   }
